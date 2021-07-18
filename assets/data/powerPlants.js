8c34b7fc2b9f07b2776eb50c8834791f84d70cb5
Aaron Yang <zhenhuayang@outlook.com>
2021-07-18 15:02:23 -0500

    power plant data added

---
+++ assets/data/powerPlants.js
@@ -0,0 +1,599 @@
+powerPlants = {
+    "type": "FeatureCollection",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [ {
+        "type": "Feature",
+        "properties": {
+            "Name": "American Crystal Sugar - Drayton",
+            "Location": "Pembina County",
+            "Capacity": "6.0",
+            "Refs": "[9]",
+            "YearOpened": "1965",
+            "FuelType": "Coal"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-97.1761, 48.5932]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "American Crystal Sugar - Hillsboro",
+            "Location": "Traill County",
+            "Capacity": "13.3",
+            "Refs": "[10]",
+            "YearOpened": "1990",
+            "FuelType": "Coal"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-97.0631, 47.4376]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Antelope Valley Station",
+            "Location": "Mercer County",
+            "Capacity": "900",
+            "Refs": "[11]",
+            "YearOpened": "1984/1986",
+            "FuelType": "Coal"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.8357, 47.3705]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Coal Creek Station",
+            "Location": "McLean County",
+            "Capacity": "1150",
+            "Refs": "[12]",
+            "YearOpened": "1979/1980",
+            "Notes": "retirement in 2022[13]",
+            "FuelType": "Coal"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.1571, 47.3777]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Coyote Station",
+            "Location": "Mercer County",
+            "Capacity": "430",
+            "Refs": "[14]",
+            "YearOpened": "1981",
+            "FuelType": "Coal"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.8157, 47.2214]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Leland Olds Station",
+            "Location": "Mercer County",
+            "Capacity": "650",
+            "Refs": "[15]",
+            "YearOpened": "1966/1975",
+            "FuelType": "Coal"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.3212, 47.2808]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Milton R. Young Station",
+            "Location": "Oliver County",
+            "Capacity": "680",
+            "Refs": "[16]",
+            "YearOpened": "1970/1977",
+            "FuelType": "Coal"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.2131, 47.0659]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "R.M. Heskett Station",
+            "Location": "Morton County",
+            "Capacity": "100",
+            "Refs": "[17]",
+            "YearOpened": "1954/1963",
+            "Notes": "retirement in 2021[18]",
+            "FuelType": "Coal"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-100.8836, 46.8669]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Coal Creek Plant",
+            "Location": "McLean County",
+            "Capacity": "6.0",
+            "FuelType": "petroleum",
+            "GenerationType": "Reciprocating Engine (x2)",
+            "Number OfUnits": "2",
+            "YearOpened": "2016"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.1571, 47.3778]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Grafton Plant",
+            "Location": "Walsh County",
+            "Capacity": "4.0",
+            "FuelType": "petroleum",
+            "GenerationType": "Reciprocating Engine (x4)",
+            "Number OfUnits": "4",
+            "YearOpened": "1937/1949/1956"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-97.4033, 48.43]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Hillsboro Plant",
+            "Location": "Traill County",
+            "Capacity": "4.0",
+            "FuelType": "petroleum",
+            "GenerationType": "Reciprocating Engine (x2)",
+            "Number OfUnits": "2",
+            "YearOpened": "2002"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-97.0606, 47.397]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Jamestown GT",
+            "Location": "Stutsman County",
+            "Capacity": "41.7",
+            "FuelType": "petroleum",
+            "GenerationType": "Simple Cycle (x2)",
+            "Number OfUnits": "2",
+            "YearOpened": "1976/1978"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-98.6622, 46.9057]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Lonesome Creek Station",
+            "Location": "McKenzie County",
+            "Capacity": "200",
+            "FuelType": "gas",
+            "GenerationType": "Simple Cycle (x5)",
+            "Number OfUnits": "5",
+            "Refs": "[19]",
+            "YearOpened": "2013/2015/2017"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-103.5786, 47.7967]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Pioneer Generating Station",
+            "Location": "Williams County",
+            "Capacity": "337",
+            "FuelType": "gas,petroleum",
+            "GenerationType": "Simple Cycle (x3)Reciprocating Engine (x12)",
+            "Number OfUnits": "15",
+            "Refs": "[20]",
+            "YearOpened": "2014/2017"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-103.9528, 48.2326]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "R.M. Heskett GT",
+            "Location": "Morton County",
+            "Capacity": "88",
+            "FuelType": "gas",
+            "GenerationType": "Simple Cycle",
+            "Number OfUnits": "1",
+            "Refs": "[21]",
+            "YearOpened": "2014"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-100.8836, 46.8669]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Spiritwood Station",
+            "Location": "Stutsman County",
+            "Capacity": "100",
+            "FuelType": "gas",
+            "GenerationType": "Steam    Turbine",
+            "Number OfUnits": "1",
+            "Refs": "[22][23]",
+            "YearOpened": "2014",
+            "Notes": "originally coal-fired.convert to gas in 2017.[24]"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-98.4997, 46.9264]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Garrison Hydro Power Plant",
+            "Location": "Mercer County",
+            "Capacity": "510",
+            "Refs": "[25]",
+            "YearOpened": "1956/1960",
+            "Notes": "USACE owned and managed[26]",
+            "FuelType": "Hydro"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.4115, 47.4945]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Ashtabula Wind Farm (I-III)",
+            "Location": "Barnes CountyGriggs CountySteele County",
+            "Capacity": "378",
+            "TurbineSpec": "GE",
+            "Numberof Units": "250",
+            "Refs": "[28]",
+            "Yearopened": "2008/2009/2010",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-98, 46.65]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Baldwin Wind Farm (I-II)(aka Wilton Wind Farm)",
+            "Location": "Burleigh County",
+            "Capacity": "99",
+            "TurbineSpec": "GE",
+            "Numberof Units": "66",
+            "Refs": "[29]",
+            "Yearopened": "2005/2006/2010",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-100.5, 47.05]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Bison Wind Energy Center (I-IV)",
+            "Location": "Morton County",
+            "Capacity": "497",
+            "TurbineSpec": "Siemens",
+            "Numberof Units": "165",
+            "Refs": "[30][31]",
+            "Yearopened": "2010/2012/2015",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.283, 47.167]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Border Winds Wind Farm",
+            "Location": "Rolette County",
+            "Capacity": "150",
+            "TurbineSpec": "Vestas",
+            "Numberof Units": "75",
+            "Refs": "[32]",
+            "Yearopened": "2016",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-99.567, 48.983]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Brady Wind Farm (I-II)",
+            "Location": "Stark County",
+            "Capacity": "299",
+            "TurbineSpec": "GE",
+            "Numberof Units": "159",
+            "Refs": "[33]",
+            "Yearopened": "2016",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-102.767, 46.7]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Cedar Hills Wind Farm",
+            "Location": "Bowman County",
+            "Capacity": "19.5",
+            "TurbineSpec": "GE",
+            "Numberof Units": "13",
+            "Refs": "[34]",
+            "Yearopened": "2010",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-103.767, 46.25]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Courtenay Wind Farm",
+            "Location": "Stutsman County",
+            "Capacity": "200",
+            "TurbineSpec": "Vestas",
+            "Numberof Units": "100",
+            "Refs": "[35][36]",
+            "Yearopened": "2016",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-98.783, 46.9]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Emmons-Logan Wind Farm",
+            "Location": "Emmons CountyLogan County",
+            "Capacity": "216",
+            "TurbineSpec": "GE",
+            "Numberof Units": "123",
+            "Yearopened": "2019",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-99.933, 46.417]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Glen Ullin Energy Center",
+            "Location": "Morton CountyMercer County",
+            "Capacity": "107",
+            "TurbineSpec": "GE",
+            "Numberof Units": "43",
+            "Yearopened": "2019",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.817, 46.967]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Langdon Wind Energy Center",
+            "Location": "Cavalier County",
+            "Capacity": "200",
+            "TurbineSpec": "GE",
+            "Numberof Units": "133",
+            "Refs": "[27]",
+            "Yearopened": "2007/2008",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-98.517, 46.1]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Lindahl Wind Farm",
+            "Location": "Williams County",
+            "Capacity": "150",
+            "TurbineSpec": "Vestas",
+            "Numberof Units": "75",
+            "Refs": "[37][38]",
+            "Yearopened": "2017",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-102.933, 48.4]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Luverne Wind Farm(adjacent to Ashtabula)",
+            "Location": "Steele County",
+            "Capacity": "49.5",
+            "TurbineSpec": "GE",
+            "Numberof Units": "33",
+            "Refs": "[39]",
+            "Yearopened": "2009",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-98.1, 47.417]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "New Frontier Wind",
+            "Location": "McHenry County",
+            "Capacity": "99",
+            "TurbineSpec": "Vestas",
+            "Numberof Units": "29",
+            "Refs": "[40]",
+            "Yearopened": "2018",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-100.8, 48.067]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "North Dakota Wind (I-II)",
+            "Location": "LaMoure County",
+            "Capacity": "61.5",
+            "TurbineSpec": "GE",
+            "Numberof Units": "41",
+            "Refs": "[27]",
+            "Yearopened": "2003",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-98.867, 46.333]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Oliver Wind Energy Center (I-II)",
+            "Location": "Oliver County",
+            "Capacity": "198",
+            "TurbineSpec": "Siemens,GE",
+            "Numberof Units": "102",
+            "Refs": "[27]",
+            "Yearopened": "2006/2007/2017",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.333, 47.15]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Prairie Winds Wind Farm (I-II)",
+            "Location": "Ward County",
+            "Capacity": "122.6",
+            "TurbineSpec": "GE,Nordex",
+            "Numberof Units": "82",
+            "Refs": "[27]",
+            "Yearopened": "2002/2009",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-101.283, 48.233]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Rugby Wind Farm",
+            "Location": "Pierce County",
+            "Capacity": "149.1",
+            "TurbineSpec": "Suzlon",
+            "Numberof Units": "71",
+            "Refs": "[27]",
+            "Yearopened": "2009",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-100, 48.367]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Sunflower Wind Farm",
+            "Location": "Morton County",
+            "Capacity": "104",
+            "TurbineSpec": "Vestas",
+            "Numberof Units": "52",
+            "Refs": "[27]",
+            "Yearopened": "2016",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-102.067, 46.833]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Tatanka Wind Farm",
+            "Location": "Dickey CountyMcIntosh County",
+            "Capacity": "91.5*",
+            "TurbineSpec": "Acciona",
+            "Numberof Units": "65",
+            "Refs": "[27]",
+            "Yearopened": "2008",
+            "Note": "* portion in North Dakota",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-98.967, 45.933]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Thunder Spirit Wind Farm (I-II)",
+            "Location": "Adams County",
+            "Capacity": "155.5",
+            "TurbineSpec": "Nordex",
+            "Numberof Units": "59",
+            "Refs": "[41][42][43]",
+            "Yearopened": "2015/2018",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-102.517, 46.317]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "Name": "Velva Wind Farm",
+            "Location": "McHenry County",
+            "Capacity": "11.9",
+            "TurbineSpec": "Vestas",
+            "Numberof Units": "18",
+            "Refs": "[27]",
+            "Yearopened": "2005",
+            "FuelType": "Wind"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-100.917, 48.083]
+        }
+    }]
+}
